7671910ae61f00e15788e7f5ca13e1b4bef81837
Vaibhav Dubey <94945041+VaibhavDubey13@users.noreply.github.com>
2026-02-26 06:28:48 +0530

    Remove ExtPay initialization from popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-const extpay = ExtPay('cyber-runner')
-
 chrome.runtime.sendMessage({ type: 'GET_HIGH_SCORE' }, (response) => {
   document.getElementById('highScore').textContent =
     response && response.highScore ? response.highScore : '0';
